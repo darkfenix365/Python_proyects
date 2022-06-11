6de6da21871a48721e4debf293c4209190297f19
Mauricio Ortiz B <mortizb@fen.uchile.cl>
2022-06-11 00:07:05 -0400

    Se agrego los indices de 5 empresas tanto para usa como europa

--- Proyecto_1.py
+++ Proyecto_1.py
@@ -5,6 +5,14 @@ import selenium as sn
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-militar_companies_list = ['HON', 'RTX', 'LMT', 'ATRO', 'AJRD', 'NOC', 'TXT', 'HEI','HII']
-data = yf.download(militar_companies_list, start = '2022-01-01', end = '2022-05-31')
+usa_militar_companies = ['LMT', 'RTX', 'GD', 'NOC', 'HII']
+europe_militar_companies = ['AIR.PA', 'BAESY', 'HO.PA', 'RHM.DE', 'LDO.MI']
 
+data_usa    = yf.download(usa_militar_companies, start = '2022-01-01', end = '2022-05-31')
+data_europe = yf.download(europe_militar_companies, start = '2022-01-01', end = '2022-05-31')
+
+pd_usa = pd.DataFrame(data_usa)
+pd_europe =pd.DataFrame(data_europe)
+
+pd_usa.to_excel("C:/Users/Mauricio Ortiz/Downloads/usa_data.xlsx")
+pd_europe.to_excel("C:/Users/Mauricio Ortiz/Downloads/europe_data.xlsx")
